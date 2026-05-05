88d9e169789adc8106fbb8c1f176ada222e9dbb4
Seva Kavalenka <seva.kavalenka@gmail.com>
2026-05-05 17:23:51 +0200

    init: start a project with webpack, react, babel, eslint, and prettier

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+import path from "node:path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+
+  module: {
+    rules: [
+      { test: /\.(js|jsx)$/, exclude: /node_modules/, use: "babel-loader" },
+    ],
+  },
+
+  plugins: [new HtmlWebpackPlugin({ template: "./public/index.html" })],
+};
