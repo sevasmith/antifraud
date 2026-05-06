a8382836e2b2d123fcff7ad34b936bd30374ebeb
Seva Kavalenka <seva.kavalenka@gmail.com>
2026-05-06 11:35:29 +0200

    feat: add sass and webpack modes

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,10 @@ export default {
   module: {
     rules: [
       { test: /\.(js|jsx)$/, exclude: /node_modules/, use: "babel-loader" },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
     ],
   },
 
