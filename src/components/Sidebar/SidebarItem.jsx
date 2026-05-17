0bd0cbde70ea8b4a5c35fef47c78cf802bc73667
Seva Kavalenka <seva.kavalenka@gmail.com>
2026-05-17 11:52:42 +0200

    feat: implement narrow sidebar design

--- src/components/Sidebar/SidebarItem.jsx
+++ src/components/Sidebar/SidebarItem.jsx
@@ -1,23 +1,31 @@
-import { useNavigate } from 'react-router';
+import { NavLink } from 'react-router';
 import listEmptyIcon from '../../assets/icons/list-empty.svg';
 
 export const SidebarItem = ({ label, icon, hasDropdown, link }) => {
-  const navigate = useNavigate();
-
-  return (
-    <div
-      className="sidebar-item"
-      onClick={() => (link ? navigate(link) : null)}
-    >
+  const content = (
+    <>
       <div className="icon-container">
         <img src={icon} alt={`${label} Icon`} />
       </div>
-      <p>{label}</p>
-      {hasDropdown ? (
+      <p className='sidebar-label'>{label}</p>
+      {hasDropdown && (
         <div className="list-icon-container">
           <img src={listEmptyIcon} alt="List Icon" />
         </div>
-      ) : null}
-    </div>
+      )}
+    </>
+  );
+
+  if (!link) {
+    return <div className="sidebar-item">{content}</div>;
+  }
+
+  return (
+    <NavLink
+      to={link}
+      className={({ isActive }) => `sidebar-item ${isActive ? 'active' : ''}`}
+    >
+      {content}
+    </NavLink>
   );
 };
