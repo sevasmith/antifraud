339ce2eb11daeb59c52c7cd63ac5c708cf62b253
Seva Kavalenka <seva.kavalenka@gmail.com>
2026-05-19 08:02:13 +0200

    Design fixes (#9)
    * fix: fix responsive design for dashboard and reports pages
    
    * fix: fix responsive design on Transactions page

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,9 @@ import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
   {
+    ignores: ['dist/', 'build/', 'node_modules/'],
+  },
+  {
     files: ['**/*.{js,mjs,cjs,jsx}'],
     plugins: { js },
     extends: ['js/recommended'],
